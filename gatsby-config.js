9352b74654ceedaccaec9367ecc9cbd2e9a5161a
Naveen Kharwar <naveen919827@gmail.com>
2020-05-10 19:13:45 +0530

    robots.txt fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
         resolve: `gatsby-plugin-robots-txt`,
         options: {
-            policy: [{ userAgenct: '*', allow: '/ '}]
+            policy: [{ userAgent: '*', allow: '/' }]
         }
     },
     {
